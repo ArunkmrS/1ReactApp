45c72942ed233716ca703e5b03e16483f522652b
ArunkmrS <Arunkumars@live.in>
2019-11-02 17:34:31 +0530

    use of props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+     return (
+     <div className="App">
         <h1>Hi from React!!!</h1>
-      </div>
-    );
+        <Person name="Arun" age="28"></Person>
+        <Person name="Shilpa" age="28">Hobbies: Drinking Tea</Person>
+        <Person name="Shubham" age="25"></Person>
+     </div>
+   );
+    //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi from React2'));
   }
 }
 
