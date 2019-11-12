e9af921a9d006b90500d21eda1b6c248fc190ba5
ArunkmrS <Arunkumars@live.in>
2019-11-13 00:59:18 +0530

    Use os states

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:"Arun", age: 28},
+      { name:"Aditya", age: 23},
+      { name:"Shubham", age: 26}
+    ]
+  }
+
   render() {
      return (
      <div className="App">
         <h1>Hi from React!!!</h1>
         <button>Switch</button>
-        <Person name="Arun" age="28"></Person>
-        <Person name="Aditya" age="28">Hobbies: Drinking Tea</Person>
-        <Person name="Shubham" age="25"></Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Drinking Tea</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
      </div>
    );
     //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi from React2'));
